4621df713b0be04ce19621de8e14aab1eddbd730
mario.rios_joyfe <mario.rios@iepgroup.es>
2026-09-16 20:30:38 +0200

    Inicial

---
+++ app/src/main/java/com/mrh/calculadora/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.mrh.calculadora
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mrh.calculadora.ui.theme.CalculadoraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculadoraTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding), texto = "Hola Clase")
+                }
+            }
+        }
+    }
+}
+
+/**
+ *  Funcion HomeScreen
+ *
+ *  @author Mario Ríos
+ *
+ *  @param modifier Modificador de un elemento visual (solo aplicar en el primer contenedor de la pantalla[Column o Row])
+ *  @param texto Texto que se pinta en pantalla
+ *
+ */
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    texto: String
+){
+
+    Column(
+        modifier = modifier
+    ){
+        Text(text = texto)
+    }
+}
